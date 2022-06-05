5d861584623945078d14c960de06684b1935a571
JoaoTomaas <joao.miguel.tomas@hotmail.com>
2022-06-05 19:17:41 +0100

    Added cards with images on the side

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -8,6 +8,25 @@ import { Component, OnInit } from '@angular/core';
 export class HomepageComponent implements OnInit {
   showFiller = false;
 
+  array: any[] = [    {
+    "name": "First post",
+    "description": "teste"
+  },
+  {
+    "name": "Second post"
+  },
+  {
+    "name": "Third post"
+  },
+  {
+    "name": "Fourth post"
+  },
+  {
+    "name": "Fifth post"
+  }
+  ]
+
+  longText = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vel aliquam erat. Ut id lacus sagittis, faucibus augue sed, varius dolor. Nam non mauris arcu. Proin a euismod nisl, ac molestie risus. Duis vel nisi nisl. Aenean gravida vel sem dictum dignissim. Nulla tristique varius laoreet. Nulla facilisi. Etiam sit. `;
 
   constructor() { }
 
